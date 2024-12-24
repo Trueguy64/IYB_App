ba9e6b1fce6a4d6f11a8e36bc326d09ed9682f0a
Trueguy64 <ronaldkok@duck.com>
2024-12-24 11:17:22 +0700

    Added New Activity

--- app/src/main/java/com/example/iyb_app/userNew.java
+++ app/src/main/java/com/example/iyb_app/userNew.java
@@ -2,41 +2,35 @@ package com.example.iyb_app;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Button;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import android.widget.TextView;
-import android.content.Intent;
 
-
-public class MainActivity extends AppCompatActivity {
+public class userNew extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.startpage), (v, insets) -> {
+        setContentView(R.layout.activity_user_new);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
     }
-    public void startSaving(View start)
+    public void handleText(View text)
     {
-        findViewById(R.id.start).setEnabled(false);
-        ((Button)findViewById(R.id.start)).setText("Saving Now");
-        Intent i = new Intent(this, userNew.class);
-        startActivity(i);
-//        start.setEnabled(false);
-//        Button button = (Button) start;
-//        button.setText("Disabled");
-
+        EditText t = findViewById(R.id.source);
+        String input = t.getText().toString();
+        ((TextView)findViewById(R.id.output)).setText(input);
+        Toast.makeText(this, input, Toast.LENGTH_LONG).show();
     }
-
 }
