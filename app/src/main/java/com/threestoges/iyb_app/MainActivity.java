6a82d2989a7f2b86cf108a0a41f0fae239e65b8f
Trueguy64 <99567745+Trueguy64@users.noreply.github.com>
2025-01-22 10:39:36 +0700

    Update MainActivity.java

--- app/src/main/java/com/threestoges/iyb_app/MainActivity.java
+++ app/src/main/java/com/threestoges/iyb_app/MainActivity.java
@@ -10,6 +10,9 @@ import androidx.core.splashscreen.SplashScreen;
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+    // Declares the path and filename of the user data
+    private final String FILENAME = "saved-data.txt";
+    private final String filepath = "user";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,16 +23,19 @@ public class MainActivity extends AppCompatActivity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
         getWindow().setStatusBarColor(Color.TRANSPARENT);
     }
+    //Reponsible for the "Start Saving" button functionality 
     public void startSaving(View start)
     {
-        String FILENAME = "saved-data.txt";
-        String filepath = "user";
+        // Declares an File object that points to the location of the file
         File userData = new File(getExternalFilesDir(filepath), FILENAME);
         findViewById(R.id.start).setEnabled(false);
         ((Button)findViewById(R.id.start)).setText("Saving Now");
+        // Creates two Intent object -a messaging object- which is used to start an activity by requesting an action 
         Intent w = new Intent(this, welcometoiyb.class);
         Intent l = new Intent(this, lucasclass.class);
+        // If the userdata does not exist then the intent will start welcometoiyb.class activity, else lucasclass.class will start
         startActivity(!userData.exists() ? w : l);
+        //Kills the activity to prevent users from going back to this activity and save memory
         finish();
     }
 }
